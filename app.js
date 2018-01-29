ddb50d9157364e7e4c5bd5e4bea91cf634f1c6ec
Tran Huy Tiep <tranhuytiep95@gmail.com>
2018-01-29 11:43:10 +0700

    11:42

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash')
 
 var array_rounter = require('./router')
 
@@ -12,6 +15,22 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//session
+app.use(session({
+    secret : "huytiep",
+    saveUninitialized: true,
+    resave: true,
+    name: 'HuyTiep',
+    // cookie: {
+    //     httpOnly: true,
+    //     secure: true
+    // }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash())
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'logo-icon.png')));
--- routes/side/user/login.js
+++ routes/side/user/login.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.route('/dangky')
     .get(function(req, res, next) {
         res.render('side/user/dangky');
     })
-    .post(function (req,res,next) {
-        var data_req = req.body
-
-    })
+    .post(passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }))
 ;
 
 
