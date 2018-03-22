376a3a31ac6fdf2ce710c1ce5c2d5807a3bcdd4c
Tran Huy Tiep <tranhuytiep95@gmail.com>
2018-03-22 17:09:41 +0700

    cryto-js

--- routes/side/user/createCert.js
+++ routes/side/user/createCert.js
@@ -7,6 +7,7 @@ var ecc = require('../../../help/ECC/ECC');
 var sendMail = require('../../../help/send_mail');
 var CryptoJS = require("crypto-js");
 var help = require('../../../help/helper')
+var config = require('../../../config/config')
 
 /*Edit Don Hang*/
 router.route('/cert/dangky')
@@ -23,8 +24,10 @@ router.route('/cert/dangky')
         let passPhrase = data.passphrase;
         let date = new Date()
         let timeCreate = date.getTime()
-        var priva = '24759591462846120382401544067961915304144449666759304319461423667541017549863'
-        var public ='483660952981732867406743740858667548923206169871659386247570893174275333083890'
+        var private_key  = CryptoJS.AES.decrypt(fs.readFileSync(config.root_dir+config.jwt.private_key).toString(),config.jwt.password);
+        var priva = private_key.toString(CryptoJS.enc.Utf8);
+        var public_key  = CryptoJS.AES.decrypt(fs.readFileSync(config.root_dir+config.jwt.public_key).toString(),config.jwt.password);
+        var public = private_key.toString(CryptoJS.enc.Utf8);
         let cert = ecc.create_cert(data.Identity,data.R,priva,public)
         data.cert = JSON.stringify(cert)
         data.timeCreate = timeCreate
@@ -72,18 +75,3 @@ router.route('/privateKey')
     });
 
 module.exports = router;
-
-//
-// //
-// //
-// // // Encrypt
-// var ciphertext = CryptoJS.AES.encrypt('my message', '1Aa23456');
-//
-// // Decrypt
-// var r  = CryptoJS.AES.decrypt('U2FsdGVkX19UKp44TK1ndKz9CQGh2Uwwd0WBkLkdUxhFs6yjGjOakflIq64rNOb5LXfThEDxbwgab6uN5yWmJppCU2Tn8O9cZ+zyVhJH5iQ81/yF6zhypFxosF7mIlnI', 'Aa123456');
-// var plaintext = r.toString(CryptoJS.enc.Utf8);
-// var cert  = CryptoJS.AES.decrypt('U2FsdGVkX1/FwwG/qNIxdnLhkBBQiPoSiGKgtY/zdf6vz1YeLWfBTg6iTKCPpah0G+1EqT9n3hxBsxEJB3sLQ1dHA7TDtyIUaZlqDAUlMYvYmobrAlQzo+0fbCbP0E9IRmLmnzPWsMqCiRVXW3K9MTXbzOzcj/I9R0zkqUOpM98huB7NbV0Cj+kj3irWep7baSUD4KGWWl0WKzF8aIjOjpxc92+1uUKs+iayy+JDlNwdH8+aho5J/JTvNsgYR5wD07r+73KPrt6H2uvYVmBi5biR1K0Rwnoroc43zp//uIM3yi5c3EORUyPR6TO/SwvSP0D8JrasZkQ2Rzrv+bXpe8/dldZSgCgB3FFLqcD9JbI4veyByUc0Z6sCWoPv+rk3HEKG3sUUO67wsrv4oyPkGWBRCs1tmFhw6e6RYZtRsIU=', 'Aa123456');
-// var plaintext_cert = cert.toString(CryptoJS.enc.Utf8);
-//
-// var key = ecc.create_key_to_cert(plaintext,JSON.parse(plaintext_cert))
-// console.log(key);
